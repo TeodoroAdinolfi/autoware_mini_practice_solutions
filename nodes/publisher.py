bb206e40859a53e72a8bda1a232a8ecd27f5c5f3
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-07-28 13:47:45 +0300

    added launch file and parameters

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -3,9 +3,10 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
 pub = rospy.Publisher('/message', String, queue_size=10)
+message = rospy.get_param('~message', 'Hello World!')
+rate = rospy.Rate(rospy.get_param('~rate','1'))
 
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
