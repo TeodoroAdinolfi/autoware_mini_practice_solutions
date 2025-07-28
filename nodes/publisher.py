e7a920e7f57a8cfac469482256e88e0a7048faf1
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-07-28 14:12:17 +0300

    just added a debug print in publisher

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -5,7 +5,9 @@ from std_msgs.msg import String
 rospy.init_node('publisher')
 pub = rospy.Publisher('/message', String, queue_size=10)
 message = rospy.get_param('~message', 'Hello World!')
-rate = rospy.Rate(rospy.get_param('~rate','1'))
+read_rate = rospy.get_param('~rate',1)
+print("Read ",read_rate)
+rate = rospy.Rate(read_rate)
 
 while not rospy.is_shutdown():
     pub.publish(message)
