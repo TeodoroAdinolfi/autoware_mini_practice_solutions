2cbecc308ee64c1e819742623378afa7dcae9da1
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-07-29 12:38:52 +0300

    Apported required changes

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -6,7 +6,6 @@ rospy.init_node('publisher')
 pub = rospy.Publisher('/message', String, queue_size=10)
 message = rospy.get_param('~message', 'Hello World!')
 read_rate = rospy.get_param('~rate',1)
-print("Read ",read_rate)
 rate = rospy.Rate(read_rate)
 
 while not rospy.is_shutdown():
