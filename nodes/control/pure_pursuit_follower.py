a043e45ed7f5ac61a358d125bb87131800489dee
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-07-30 12:42:06 +0300

    point 4 - ok

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -4,28 +4,40 @@ import rospy
 from autoware_mini.msg import Path
 from geometry_msgs.msg import PoseStamped
 from autoware_mini.msg import VehicleCmd
+from shapely.geometry import LineString, Point
+from shapely import prepare, distance
 
 class PurePursuitFollower:
     def __init__(self):
-
+        
         # Parameters
-
+        self.path_linestring = None
+        
         # Publishers
         self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd',VehicleCmd,queue_size=10)
         # Subscribers
         rospy.Subscriber('path', Path, self.path_callback, queue_size=1)
         rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1)
 
-    def path_callback(self, msg):
-        pass
+    def path_callback(self, msg : Path):
+        # convert waypoints to shapely linestring
+        self.path_linestring = LineString([(w.position.x, w.position.y) for w in msg.waypoints])
+        # prepare path - creates spatial tree, making the spatial queries more efficient
+        prepare(self.path_linestring)
 
     def current_pose_callback(self, msg : PoseStamped):
         # print("X: ",msg.pose.position.x, "Y: ", msg.pose.position.y)
         vehicle_cmd = VehicleCmd()
-        vehicle_cmd.ctrl_cmd.steering_angle = 0.2
-        vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
+        #vehicle_cmd.ctrl_cmd.steering_angle = 0.2
+        #vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
         vehicle_cmd.header.stamp = msg.header.stamp
         vehicle_cmd.header.frame_id = "base_link"
+        
+        current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+        if (self.path_linestring is not None):
+            d_ego_from_path_start = self.path_linestring.project(current_pose)
+            print("ego:", d_ego_from_path_start)
+        
         self.vehicle_cmd_pub.publish(vehicle_cmd)
 
     def run(self):
