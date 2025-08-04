ffbc50550d1d20a7644c37a81ca9fdab34c9ca08
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-08-04 13:12:28 +0300

    Practice 5 - step 3
    clastering added and parameters read

--- nodes/detection/cluster_detector.py
+++ nodes/detection/cluster_detector.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import PointCloud2
 from ros_numpy import numpify
 from numpy.lib.recfunctions import structured_to_unstructured, unstructured_to_structured
 from ros_numpy import numpify, msgify
+from sklearn.cluster import DBSCAN 
 import numpy as np
 
 
@@ -13,9 +14,10 @@ class PointsClusterer:
 
     def __init__(self):
         # Parameters
-        
+        self.cluster_epsilon  = rospy.get_param("~/detection/lidar/points_clusterer/cluster_epsilon")
+        self.cluster_min_size = rospy.get_param("~/detection/lidar/points_clusterer/cluster_min_size")
         # Internal Variables
-
+        self.clusterer = DBSCAN(self.cluster_epsilon,self.cluster_min_size)
         # Subscribers
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2**24, tcp_nodelay=True)
 
@@ -25,6 +27,8 @@ class PointsClusterer:
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
         print("Points shape", points.shape)
+        labels = self.clusterer.fit_predict(points)
+        print("Labels shape", labels.shape)
 
 
     def run(self):
