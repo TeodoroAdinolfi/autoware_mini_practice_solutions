38953c284d5d10a4f342ead75a7c451bf34631da
Teodoro <t.adinolfi2@studenti.unisa.it>
2025-08-04 13:39:53 +0300

    Practice 5 - step 4
    now the cluster is efectively done

--- nodes/detection/cluster_detector.py
+++ nodes/detection/cluster_detector.py
@@ -18,17 +18,39 @@ class PointsClusterer:
         self.cluster_min_size = rospy.get_param("~/detection/lidar/points_clusterer/cluster_min_size")
         # Internal Variables
         self.clusterer = DBSCAN(self.cluster_epsilon,self.cluster_min_size)
+        # Publishers
+        self.points_clustered_pub = rospy.Publisher('/detection/lidar/points_clustered',PointCloud2,queue_size=1,tcp_nodelay=True)
+
         # Subscribers
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2**24, tcp_nodelay=True)
 
-        # Publishers
-
+        
     def points_callback(self, msg : PointCloud2):
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
         print("Points shape", points.shape)
         labels = self.clusterer.fit_predict(points)
         print("Labels shape", labels.shape)
+        # creating points_labeled a single tensor with filtered rows
+        labels_reshaped = labels.reshape(-1, 1)
+        combined_tensor = np.concatenate((points, labels_reshaped), axis=1)
+        mask = combined_tensor[:, 3] != -1
+        points_labeled = combined_tensor[mask]
+
+        # convert labelled points to PointCloud2 format
+        data = unstructured_to_structured(points_labeled, dtype=np.dtype([
+        ('x', np.float32),
+        ('y', np.float32),
+        ('z', np.float32),
+        ('label', np.int32)
+        ]))
+        # publish clustered points message
+        cluster_msg = msgify(PointCloud2, data)
+        cluster_msg.header.frame_id = msg.header.frame_id
+        cluster_msg.header.stamp = msg.header.stamp
+        self.points_clustered_pub.publish(cluster_msg)
+
+
 
 
     def run(self):
